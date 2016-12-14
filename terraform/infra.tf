4b60814e9725be97f107a3b4b66cc2cd7272eb0d
Ashley Vargas <ashley.vargas.44@my.csun.edu>
2016-12-14 01:25:05 -0800

    updated

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -7,6 +7,8 @@ variable "vpc_id" {
   default = "vpc-b2f407d5"
 }
 
+variable "db_password" {}
+
 provider "aws" {
   region = "us-west-2"
 }
@@ -38,9 +40,9 @@ resource "aws_route_table" "public_routing_table" {
 
 resource "aws_subnet" "public_subnet_a" {
     vpc_id = "${var.vpc_id}"
-    cidr_block = "172.31.0.0/24"
-    availability_zone = "us-west-2a"
+    cidr_block = "172.31.15.0/24"
     map_public_ip_on_launch = true
+    availability_zone = "us-west-2a"
 
     tags {
         Name = "public_a"
@@ -49,9 +51,9 @@ resource "aws_subnet" "public_subnet_a" {
 
 resource "aws_subnet" "public_subnet_b" {
     vpc_id = "${var.vpc_id}"
-    cidr_block = "172.31.1.0/24"
-    availability_zone = "us-west-2b"
+    cidr_block = "172.31.16.0/24"
     map_public_ip_on_launch = true
+    availability_zone = "us-west-2b"
 
     tags {
         Name = "public_b"
@@ -60,9 +62,9 @@ resource "aws_subnet" "public_subnet_b" {
 
 resource "aws_subnet" "public_subnet_c" {
     vpc_id = "${var.vpc_id}"
-    cidr_block = "172.31.2.0/24"
-    availability_zone = "us-west-2c"
+    cidr_block = "172.31.17.0/24"
     map_public_ip_on_launch = true
+    availability_zone = "us-west-2c"
 
     tags {
         Name = "public_c"
@@ -74,7 +76,7 @@ resource "aws_subnet" "public_subnet_c" {
 
 resource "aws_subnet" "private_subnet_a" {
     vpc_id = "${var.vpc_id}"
-    cidr_block = "172.31.100.0/22"
+    cidr_block = "172.31.0.0/22"
     availability_zone = "us-west-2a"
 
     tags {
@@ -84,7 +86,7 @@ resource "aws_subnet" "private_subnet_a" {
 
 resource "aws_subnet" "private_subnet_b" {
     vpc_id = "${var.vpc_id}"
-    cidr_block = "172.31.150.0/22"
+    cidr_block = "172.31.5.0/22"
     availability_zone = "us-west-2b"
 
     tags {
@@ -94,7 +96,7 @@ resource "aws_subnet" "private_subnet_b" {
 
 resource "aws_subnet" "private_subnet_c" {
     vpc_id = "${var.vpc_id}"
-    cidr_block = "172.31.200.0/22"
+    cidr_block = "172.31.10.0/22"
     availability_zone = "us-west-2c"
 
     tags {
@@ -104,23 +106,23 @@ resource "aws_subnet" "private_subnet_c" {
 
 resource "aws_eip" "elastic_ip" {
   vpc      = true
-  depends_on = ["aws_internet_gateway.gw"]
+#  depends_on = ["aws_internet_gateway.gw"]
 }
 
 
 resource "aws_nat_gateway" "gw" {
     allocation_id = "${aws_eip.elastic_ip.id}"
     subnet_id = "${aws_subnet.public_subnet_a.id}"
-    depends_on = ["aws_internet_gateway.gw"]
+#    depends_on = ["aws_internet_gateway.gw"]
 }
 
 
 resource "aws_route_table" "private_routing_table" {
   vpc_id = "${var.vpc_id}"
   route {
-	cidr_block = "0.0.0.0/0"
-	gateway_id = "${aws_nat_gateway.gw.id}"
-	}
+  cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_nat_gateway.gw.id}"
+  }
     tags {
     Name = "private_routing_table"
   }
@@ -175,9 +177,11 @@ resource "aws_security_group" "allow_local" {
       from_port = 22
       to_port = 22
       protocol = "tcp"
- #	  cidr_blocks = ["0.0.0.0/0"]
-      cidr_blocks = ["172.88.16.73/32"]
+     cidr_blocks = ["0.0.0.0/0"]
+ #     cidr_blocks = ["172.31.0.0/16"]
+  #    cidr_blocks = ["130.166.220.26/16"]
   }
+
   egress {
     from_port = 0
     to_port = 0
@@ -195,12 +199,11 @@ resource "aws_security_group" "allow_local" {
 
 # Create a new instance Amazon Linux AMI 2016.09.0 HVM (SSD) EBS-Backed 64-bit
 # t2.micro node
-resource "aws_instance" "web" {
+resource "aws_instance" "bastion" {
     ami = "ami-5ec1673e"
     instance_type = "t2.micro"
     vpc_security_group_ids = ["${aws_security_group.allow_local.id}"]
     subnet_id = "${aws_subnet.public_subnet_a.id}"
-#   availability_zone = "us-west-2a"
     associate_public_ip_address = true
     key_name = "cit360"
 
@@ -213,32 +216,20 @@ resource "aws_instance" "web" {
 
 # ASSIGNMENT 3
 
-variable "db_pass" {
-  type = "string"
-  description = "db password"
-  default = "vdw4d_32l"
-}
-
 
 # * * * Security Group for DB * * * 
 
-resource "aws_security_group" "db" {
+resource "aws_security_group" "db_security" {
   name = "db_sec_group"
   vpc_id = "${var.vpc_id}"
   description = "Allow inbound ssh traffic from VPC"
 
   ingress {
-      from_port = 22
-      to_port = 22
+      from_port = 3306
+      to_port = 3306
       protocol = "tcp"
- #	  cidr_blocks = ["0.0.0.0/0"]
+ #    cidr_blocks = ["0.0.0.0/0"]
       cidr_blocks = ["172.31.0.0/16"]
-  }
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags {
@@ -249,7 +240,7 @@ resource "aws_security_group" "db" {
 
 # * * * DB Subnet Group * * * 
 
-resource "aws_db_subnet_group" "default" {
+resource "aws_db_subnet_group" "db_subnet" {
     name = "db"
     subnet_ids = ["${aws_subnet.private_subnet_a.id}", "${aws_subnet.private_subnet_b.id}"]
     tags {
@@ -260,19 +251,19 @@ resource "aws_db_subnet_group" "default" {
 
 # * * * DB Instance * * * 
 
-resource "aws_db_instance" "default" {
+resource "aws_db_instance" "database" {
   allocated_storage    = 5
   engine               = "mariadb"
   engine_version       = "10.0.24"
   instance_class       = "db.t2.micro"
   storage_type         = "gp2"
   multi_az             = "false"
-  publicly_accessible  = "false"
-  db_subnet_group_name = "${aws_db_subnet_group.default.id}"
-  identifier           = "dbinstance"
+  db_subnet_group_name = "${aws_db_subnet_group.db_subnet.id}"
   name                 = "mydb"
   username             = "dbuser"
-  password             = "${var.db_pass}"
+  password             = "${var.db_password}"
+  parameter_group_name = "default.mariadb10.0"
+  vpc_security_group_ids = ["${aws_security_group.db_security.id}"]
   
 
   tags {
@@ -293,6 +284,7 @@ resource "aws_security_group" "asmt3_instances" {
       to_port = 22
       protocol = "tcp"
       cidr_blocks = ["172.31.0.0/16"]
+      cidr_blocks = ["130.166.220.26/16"]
   }
 
   ingress {
@@ -314,14 +306,14 @@ resource "aws_security_group" "asmt3_instances" {
 }
 
 
-resource "aws_security_group" "elb" {
+resource "aws_security_group" "elb_security" {
   name = "elb_sec_group"
   vpc_id = "${var.vpc_id}"
   description = "Allow all inbound ssh traffic"
 
   ingress {
-      from_port = 22
-      to_port = 22
+      from_port = 80
+      to_port = 80
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
@@ -342,8 +334,9 @@ resource "aws_security_group" "elb" {
 
 resource "aws_elb" "webservice" {
   name = "webservice-elb"
-#  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  security_groups = ["${aws_security_group.elb.id}"]
+
+  security_groups = ["${aws_security_group.elb_security.id}"]
+
   subnets = ["${aws_subnet.public_subnet_b.id}", "${aws_subnet.public_subnet_c.id}"]
 
   listener {
@@ -362,6 +355,8 @@ resource "aws_elb" "webservice" {
   }
 
   instances = ["${aws_instance.webserver-b.id}", "${aws_instance.webserver-c.id}"]
+  cross_zone_load_balancing = true
+  idle_timeout = 60
   connection_draining = true
   connection_draining_timeout = 60
 
@@ -377,7 +372,7 @@ resource "aws_elb" "webservice" {
 resource "aws_instance" "webserver-b" {
     ami = "ami-5ec1673e"
     instance_type = "t2.micro"
-#    vpc_security_group_ids = ["${aws_security_group.allow_local.id}"]
+    vpc_security_group_ids = ["${aws_security_group.asmt3_instances.id}"]
     subnet_id = "${aws_subnet.private_subnet_b.id}"
     associate_public_ip_address = false
     key_name = "cit360"
@@ -392,7 +387,7 @@ resource "aws_instance" "webserver-b" {
 resource "aws_instance" "webserver-c" {
     ami = "ami-5ec1673e"
     instance_type = "t2.micro"
-#    vpc_security_group_ids = ["${aws_security_group.allow_local.id}"]
+    vpc_security_group_ids = ["${aws_security_group.asmt3_instances.id}"]
     subnet_id = "${aws_subnet.private_subnet_c.id}"
     associate_public_ip_address = false
     key_name = "cit360"
